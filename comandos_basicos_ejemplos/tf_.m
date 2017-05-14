1fa185476e51af0d904304b9719d5239afd595c2
danielchungara89 <danielchungara89@gmail.com>
2017-05-14 05:16:19 -0300

    instalacion paquetes

---
+++ comandos_basicos_ejemplos/tf_.m
@@ -0,0 +1,3 @@
+pkg load control
+G=tf([5 2],[1 4 20]); 
+sysout(G, 'tf')
---
+++ instalacion_paquetes.m
@@ -0,0 +1,8 @@
+#tf, sysout, tf2zp, tfdata, zp
+pkg install PKG_Octave\control-3.0.0.tar.gz
+pkg install PKG_Octave\miscellaneous-1.2.1.tar.gz
+pkg install PKG_Octave\optim-1.5.0.tar.gz
+pkg install PKG_Octave\optim-1.5.2.tar.gz
+pkg install PKG_Octave\io-2.4.7.tar.gz
+pkg install PKG_Octave\symbolic-2.5.0.tar.gz
+pkg install PKG_Octave\signal-1.3.2.tar.gz
