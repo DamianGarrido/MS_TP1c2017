1abf8ba6912e1bf863cce33365c6066d043748d8
danielchungara89 <danielchungara89@gmail.com>
2017-05-24 19:11:35 -0300

    implementadas funcionalidades restantes falta probar el ingreso de de una matriz con numeros imaginarios puros

--- consola.m
+++ consola.m
@@ -19,7 +19,15 @@ while(opcion ~= 0)
   case 5
     indicar_ganancia(ft);
   case 6
+    indicar_polos_ceros_ganancia(ft);
+  case 7
     mostrar_transferencia(ft);
+  case 8
+    mostrar_constelacion(ft);
+  case 9
+    indicar_estabilidad(ft);
+  case 10
+    mostrar_todas_caracteristicas(ft);
   case 99
     mostrar_menu
   otherwise
--- procesador_funciones.m
+++ procesador_funciones.m
@@ -29,19 +29,50 @@ function indicar_ganancia(transferencia)
  k
 endfunction
 
+function indicar_polos_ceros_ganancia(transferencia)
+ [Zer,Pol,k]=tf2zp(transferencia);
+ Zer,Pol,k
+endfunction
+
 function mostrar_transferencia(transferencia)
  transferencia
+endfunction
+
+function mostrar_constelacion(transferencia)
+ pzmap(transferencia)
+endfunction
+
+function indicar_estabilidad(transferencia)
+  [Zer,Pol,k]=tf2zp(transferencia);
+  parte_real = real(Pol);
+  if (parte_real <= 0)
+    printf("estable\n");
+  else
+    printf("inestable\n");
+  endif
+endfunction
+
+function mostrar_todas_caracteristicas(transferencia)
+  tf = transferencia;
+  mostrar_transferencia(tf);
+  indicar_polos_ceros_ganancia(tf);
+  mostrar_constelacion(tf);
+  indicar_estabilidad(tf);
 endfunction
 
 function mostrar_menu 
   fprintf("Menu Opciones\n")
   fprintf("0.Salir\n")
-  fprintf("1.Transferencia dados 2 polinomios\n")
-  fprintf("2.Transferencia dados ceros, polos y ganancia\n")
-  fprintf("3.Indicar polos de la transferencia actual\n")
-  fprintf("4.Indicar ceros de la transferencia actual\n")
-  fprintf("5.Marcar ganancia de la transferencia actual\n")
-  fprintf("6.Mostrar transferencia actual.\n")
+  fprintf("1.Ingresar transferencia dados 2 polinomios\n")
+  fprintf("2.Ingresar transferencia dados ceros, polos y ganancia\n")
+  fprintf("3.Indicar polos\n")
+  fprintf("4.Indicar ceros\n")
+  fprintf("5.Marcar ganancia\n")
+  fprintf("6.Mostrar polos, ceros y ganancia\n")
+  fprintf("7.Mostrar transferencia como cociente de polinomios.\n")
+  fprintf("8.Mostrar constelacion de polos y ceros.\n")
+  fprintf("9.Indicar estabilidad.\n")
+  fprintf("10.Obtener todas las caracteristicas de la transferencia.\n")  
   fprintf("99.Mostrar menu nuevamente\n")
 endfunction  
   
